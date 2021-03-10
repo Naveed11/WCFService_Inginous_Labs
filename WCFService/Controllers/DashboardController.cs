e103a438d1029410e353fbbe321961f3ddd9490c
Naveed11 <Naveednayyar539@gmail.com>
2021-03-10 14:50:59 +0500

    Project-10-Mar

---
+++ WCFService/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WCFService.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashborad
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
