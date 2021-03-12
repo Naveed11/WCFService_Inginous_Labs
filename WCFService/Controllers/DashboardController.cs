d03a1f352031606f6991ba5e157d0e0a15d4e534
Naveed11 <Naveednayyar539@gmail.com>
2021-03-12 11:52:25 +0500

    12 March 2021

---
+++ WCFService/Controllers/DashboardController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WCFService.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashborad
+
+        public void MachineEyes()
+        {
+            MachineEyes.MachineEyesServiceClient client = new MachineEyes.MachineEyesServiceClient();
+            MachineEyes.Sheds[] s = client.Get_Sheds("adfadf");
+            ViewBag.Shed = s;
+        }
+        //public void ChangeRequestURL()
+        //{
+        //    string fullUrl = Request.Url.ToString();
+        //    if (fullUrl.Contains("/Dashboard/Setting"))
+        //    {
+        //        Content.RewritePath("/Dashboard/Index");
+        //    }
+        //}
+        public ActionResult Index()
+        {
+            MachineEyes();
+            return View();
+        }
+
+        public ActionResult Setting()
+        {
+            MachineEyes();
+            return View();
+        }
+
+    }
+}
